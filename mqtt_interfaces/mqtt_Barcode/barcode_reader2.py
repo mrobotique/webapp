8dd87ebd40918cddfd4c7d8dc227a7c2aef5c39b
Mromero <miguel.aguas@gmail.com>
2020-10-07 20:19:00 -0500

    cleanup

--- mqtt_interfaces/mqtt_Barcode/barcode_reader2.py
+++
@@ -1,7 +0,0 @@
-from evdev import InputDevice, categorize, ecodes
-
-device = InputDevice("/dev/input/by-id/usb-WIT_Electron_Company_WIT_122-UFS_V7.13-event-kbd"
-                     "") # my keyboard
-for event in device.read_loop():
-    if event.value == ecodes.EV_KEY:
-        print(categorize(event))
