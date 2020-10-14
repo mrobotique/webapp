5cbf1b68042ccd049f81e2993c64f4ca3e60e92d
MRO <miguel.r@jcaelectronics.ca>
2020-10-14 17:43:34 -0500

    classify main.py

--- main.py
+++ main.py
@@ -13,130 +13,146 @@ import json
 import yaml
 import logManager
 
-
-ser = serial.Serial('/dev/ttyUSB0', 19200, timeout=1)
-ser.setRTS(False)
-ser.setDTR(False)
-ser.read_all()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
 async_mode = "eventlet"
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
-thread = None
-thread_lock = Lock()
-# Control Globals
-global log_entry
-global uvsa_dict
-uvsa_dict = {}
-global Ubicacion
-Ubicacion = "tst"
-global HoraInicio, lastCall, ExposureTime
 
 
-def background_serial_reader_thread():
-    global uvsa_dict
-    # pass
-    """Example of how to send server generated events to clients."""
-    last_pub = 0
-    uvsa_key=['msg_id', 'version', 'card', 'deadman1', 'deadman2', 'auto', 'pir1', 'pir2', 'pir3', 'pir4', 'mag1',
-              'mag2', 'lamp_byte', 'horometro', 'buzzer', 'operationMode', 'tiempoRestante', 'mask_byte', 'count_down',
-              'state_machine_status', 'total_time']
+class WebApp:
+    """
 
-    decoded_data = []
-    last_modo_operacion = 3 # Manera natural de iniciar el Operation Mode
+    """
 
-    while True:
-        try:
-            ser_data = ser.readline().decode('utf8')[:-2]
-        except UnicodeDecodeError:
-            pass
+    def __init__(self):
+        """
 
-        try:
-            decoded_data = ser_data.split(":")[1][1:-2].split(',')
-        except IndexError:
-            print("index error")
+        """
+        self.ser = serial.Serial('/dev/ttyUSB0', 19200, timeout=1)
+        self.ser.setRTS(False)
+        self.ser.setDTR(False)
+        self.ser.read_all()
 
-        if len(uvsa_key) == len(decoded_data): #El primer paquete despues de inicializar tiene basura... por eso se filtra aqui
-            for i in range(len(uvsa_key)):
-                try:
-                    uvsa_dict[uvsa_key[i]] = int(decoded_data[i])
-                except:
-                    if decoded_data[i] == 'true':
-                        uvsa_dict[uvsa_key[i]] = 1
-                    else:
-                        if decoded_data[i] == 'false':
-                            uvsa_dict[uvsa_key[i]] = 0
+        # Control Globals
+        self.log_entry = []
+        self.uvsa_dict = {}
+        self.Ubicacion = "tst"
+        self.HoraInicio = 0
+        self.lastCall = 0
+        self.ExposureTime = 0
+        self.thread = None
+        self.thread_lock = Lock()
+
+    def background_serial_reader_thread(self):
+        """Example of how to send server generated events to clients."""
+        last_pub = 0
+        uvsa_key=['msg_id', 'version', 'card', 'deadman1', 'deadman2', 'auto', 'pir1', 'pir2', 'pir3', 'pir4', 'mag1',
+                  'mag2', 'lamp_byte', 'horometro', 'buzzer', 'operationMode', 'tiempoRestante', 'mask_byte', 'count_down',
+                  'state_machine_status', 'total_time']
+
+        decoded_data = []
+        last_modo_operacion = 3 # Manera natural de iniciar el Operation Mode
+
+        while True:
+            try:
+                ser_data = self.ser.readline().decode('utf8')[:-2]
+            except UnicodeDecodeError:
+                pass
+
+            try:
+                decoded_data = ser_data.split(":")[1][1:-2].split(',')
+            except IndexError:
+                print("index error")
+                socketio.sleep(0.02)
+
+            if len(uvsa_key) == len(decoded_data): #El primer paquete despues de inicializar tiene basura... por eso se filtra aqui
+                for i in range(len(uvsa_key)):
+                    try:
+                        self.uvsa_dict[uvsa_key[i]] = int(decoded_data[i])
+                    except KeyError:
+                        if decoded_data[i] == 'true':
+                            self.uvsa_dict[uvsa_key[i]] = 1
                         else:
-                            uvsa_dict[uvsa_key[i]] = decoded_data[i][1:-1]
-            today = str(datetime.date(datetime.now())).split("-")
-            uvsa_dict['date'] = today[2] + "/" + today[1] + "/" + today[0] # dd-mm-yyy
-            uvsa_dict['time'] = str(datetime.time(datetime.now())).split('.')[0]
-            #Decodificar lamparas
-            uvsa_dict['lamp1'] = uvsa_dict['lamp_byte'] & 0b00000001
-            uvsa_dict['lamp2'] = uvsa_dict['lamp_byte'] & 0b00000010
-            uvsa_dict['lamp3'] = uvsa_dict['lamp_byte'] & 0b00000100
-            uvsa_dict['lamp4'] = uvsa_dict['lamp_byte'] & 0b00001000
-            uvsa_dict['lamp5'] = uvsa_dict['lamp_byte'] & 0b00010000
-            uvsa_dict['lamp6'] = uvsa_dict['lamp_byte'] & 0b00100000
-            uvsa_dict['lampDeadman'] = uvsa_dict['lamp_byte'] & 0b01000000
-            uvsa_dict['lampAuto'] = uvsa_dict['lamp_byte'] & 0b10000000
-            # Checa si es necesario guardar la ultima rutina de sanitizacion en la bitacora
-            last_modo_operacion = create_log(uvsa_dict['operationMode'], last_modo_operacion)
-            #  ToDo Un Horometro de verdad
-            socketio.sleep(0.02)
-            #print(uvsa_dict)
-            pub = int(float(uvsa_dict['msg_id'])/100)
-            if pub != last_pub:
-                last_pub = pub
-                socketio.emit('my_response', uvsa_dict, broadcast=True)
+                            if decoded_data[i] == 'false':
+                                self.uvsa_dict[uvsa_key[i]] = 0
+                            else:
+                                self.uvsa_dict[uvsa_key[i]] = decoded_data[i][1:-1]
+                today = str(datetime.date(datetime.now())).split("-")
+                self.uvsa_dict['date'] = today[2] + "/" + today[1] + "/" + today[0] # dd-mm-yyy
+                self.uvsa_dict['time'] = str(datetime.time(datetime.now())).split('.')[0]
+                #Decodificar lamparas
+                self.uvsa_dict['lamp1'] = self.uvsa_dict['lamp_byte'] & 0b00000001
+                self.uvsa_dict['lamp2'] = self.uvsa_dict['lamp_byte'] & 0b00000010
+                self.uvsa_dict['lamp3'] = self.uvsa_dict['lamp_byte'] & 0b00000100
+                self.uvsa_dict['lamp4'] = self.uvsa_dict['lamp_byte'] & 0b00001000
+                self.uvsa_dict['lamp5'] = self.uvsa_dict['lamp_byte'] & 0b00010000
+                self.uvsa_dict['lamp6'] = self.uvsa_dict['lamp_byte'] & 0b00100000
+                self.uvsa_dict['lampDeadman'] = self.uvsa_dict['lamp_byte'] & 0b01000000
+                self.uvsa_dict['lampAuto'] = self.uvsa_dict['lamp_byte'] & 0b10000000
+                # Checa si es necesario guardar la ultima rutina de sanitizacion en la bitacora
+                last_modo_operacion = self.create_log(self.uvsa_dict['operationMode'], last_modo_operacion)
+                #  ToDo Un Horometro de verdad
+                socketio.sleep(0.02)
+                #print(self.uvsa_dict)
+                pub = int(float(self.uvsa_dict['msg_id'])/100)
+                if pub != last_pub:
+                    last_pub = pub
+                    socketio.emit('my_response', self.uvsa_dict, broadcast=True)
+
+    @staticmethod
+    def incrementa_horometro(ExposureTime):
+        """
+        :param ExposureTime:
+        :type ExposureTime:
+        :return:
+        :rtype:
+        """
+        if math.floor(ExposureTime) > 0:
+            print("incrementa_horometro", ExposureTime)
+
+    def create_log(self, modo_operacion, last_modo_operacion):
+        """
+        :param modo_operacion:
+        :type modo_operacion:
+        :param last_modo_operacion:
+        :type last_modo_operacion:
+        :return:
+        :rtype:
+        """
+        if (modo_operacion == 1) and (last_modo_operacion == 2):
+            self.HoraInicio = datetime.now()
+            self.lastCall = self.HoraInicio
+            self.ExposureTime = 0
+            self.log_entry = [self.uvsa_dict['date'], self.uvsa_dict['time'], "0", self.Ubicacion]
+
+        if (modo_operacion == 3) and (last_modo_operacion == 1):
+            if self.ExposureTime > 0:
+                self.log_entry[2] = str(math.floor(self.ExposureTime))
+                logManager.add_new_entry(self.log_entry)
+                self.log_entry = []
+            socketio.emit('reload_log', broadcast=True)
+
+        try:
+            if self.lastCall == self.HoraInicio and self.uvsa_dict['state_machine_status'] == 2:
+                self.lastCall = datetime.now()
+
+            if self.lastCall != self.HoraInicio:
+                now = datetime.now()
+                delta_time = now - self.lastCall
+                self.lastCall = now
+                if self.uvsa_dict['state_machine_status'] == 2:
+                    self.ExposureTime += delta_time.total_seconds()
+                    if (self.ExposureTime % 10) < 0.2: # 0.2 Segundos ya que la frec del mesaje es de 5hz. Asi solo toma 1 msg cada 10 seg
+                        self.incrementa_horometro(self.ExposureTime)
+        except NameError:
+            pass
+        return modo_operacion
 
 
-def incrementa_horometro(ExposureTime):
-    if math.floor(ExposureTime) > 0:
-        print("incrementa_horometro", ExposureTime)
-
-
-def create_log(modo_operacion, last_modo_operacion):
-    global log_entry
-    global Ubicacion
-    global HoraInicio
-    global lastCall
-    global ExposureTime
-    global uvsa_dict
-
-    if (modo_operacion == 1) and (last_modo_operacion == 2):
-        HoraInicio = datetime.now()
-        lastCall = HoraInicio
-        ExposureTime = 0
-        log_entry = [uvsa_dict['date'], uvsa_dict['time'], "0", Ubicacion]
-
-    if (modo_operacion == 3) and (last_modo_operacion == 1):
-        if ExposureTime > 0:
-            log_entry[2] = str(math.floor(ExposureTime))
-            logManager.add_new_entry(log_entry)
-            log_entry = []
-        socketio.emit('reload_log', broadcast=True)
-
-    try:
-        if lastCall == HoraInicio and uvsa_dict['state_machine_status'] == 2:
-            lastCall = datetime.now()
-
-        if lastCall != HoraInicio:
-            now = datetime.now()
-            delta_time = now - lastCall
-            lastCall = now
-            if uvsa_dict['state_machine_status'] == 2:
-                ExposureTime += delta_time.total_seconds()
-                if (ExposureTime % 10) < 0.2: # 0.2 Segundos ya que la frec del mesaje es de 5hz. Asi solo toma 1 msg cada 10 seg
-                    incrementa_horometro(ExposureTime)
-
-    except NameError:
-        pass
-    return modo_operacion
+my_webapp = WebApp()
 
 
 @app.route('/')
@@ -177,27 +193,26 @@ def acerca_info():
     # ToDo debe de llenar.
     with open("/etc/uvsa/model_sn.yaml") as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
-    fabricacion_dict = {'serialNumber':data['Serial'], 'modelo':data['Model']}
+    fabricacion_dict = {'serialNumber': data['Serial'], 'modelo': data['Model']}
     socketio.emit('info_fabricacion', fabricacion_dict)
 
 
 @socketio.on('connect')
 def test_connect():
     #print("client connected")
-    global thread
-    with thread_lock:
-        if thread is None:
-            thread = socketio.start_background_task(background_serial_reader_thread)
+    with my_webapp.thread_lock:
+        if my_webapp.thread is None:
+            my_webapp.thread = socketio.start_background_task(my_webapp.background_serial_reader_thread())
     emit('my_response', {'data': 'Connected', 'count': 0})
 
 
 @socketio.on('startButton')
-def start_button(msg):
-    ser.write((json.dumps(msg) + '\r\n').encode())
+def start_button(self, msg):
+    self.ser.write((json.dumps(msg) + '\r\n').encode())
 
 
 @socketio.on('set_dateTime')
-def set_datetime(msg):
+def set_datetime(self, msg):
     """
     :param msg: Diccionario que contiene la hora y la fecha msg = {'fecha':str, 'hora':str}
     :type: dict
@@ -211,21 +226,19 @@ def set_datetime(msg):
 
 
 @socketio.on('request_toggle_status')
-def set_toggle_status():
-    global uvsa_dict
-    mask_byte = {}
-    mask_byte['buzzer'] = uvsa_dict['mask_byte'] & 0b00000001
-    mask_byte['lamp_1'] = uvsa_dict['mask_byte'] & 0b00000010
-    mask_byte['lamp_2'] = uvsa_dict['mask_byte'] & 0b00000100
-    mask_byte['lamp_3'] = uvsa_dict['mask_byte'] & 0b00001000
-    mask_byte['lamp_4'] = uvsa_dict['mask_byte'] & 0b00010000
-    mask_byte['lamp_5'] = uvsa_dict['mask_byte'] & 0b00100000
-    mask_byte['lamp_6'] = uvsa_dict['mask_byte'] & 0b01000000
+def set_toggle_status(self):
+    mask_byte = {'buzzer': my_webapp.uvsa_dict['mask_byte'] & 0b00000001,
+                 'lamp_1': my_webapp.uvsa_dict['mask_byte'] & 0b00000010,
+                 'lamp_2': my_webapp.uvsa_dict['mask_byte'] & 0b00000100,
+                 'lamp_3': my_webapp.uvsa_dict['mask_byte'] & 0b00001000,
+                 'lamp_4': my_webapp.uvsa_dict['mask_byte'] & 0b00010000,
+                 'lamp_5': my_webapp.uvsa_dict['mask_byte'] & 0b00100000,
+                 'lamp_6': my_webapp.uvsa_dict['mask_byte'] & 0b01000000}
     emit('set_toggle_status', mask_byte)
 
 
 @socketio.on('delete_entries')
-def delete_entries(entries_list):
+def delete_entries(self, entries_list):
     logManager.del_rows(entries_list)
     emit('reload_log', broadcast=True)
 
